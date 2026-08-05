aa02dc53d3545f135c756ccd89a778be66c2fd00
pierodm20 <donayrep20@gmail.com>
2026-08-04 20:58:06 -0500

    Primero

---
+++ ProyectoMoviles2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoMoviles2
+//
+//  Created by DESIGN on 4/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
